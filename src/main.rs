714f1055207dbfa12d325abc2b5d415677e16228
Patrick Lam <prof.lam@gmail.com>
2025-05-28 11:56:21 +1200

    handle directories

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,9 @@ use syn::visit::Visit;
 
 use std::env;
 use std::fs;
+use std::io;
 use std::process;
+use std::path::Path;
 
 struct StmtVisitor {
     found_unsafe: bool,
@@ -119,26 +121,77 @@ fn print_trait_unsafe_containing_fns(it: ItemTrait) {
     }
 }
 
+fn handle_file(path:&Path) {
+    println!("# Unsafe usages in file {}", path.display());
+    if !path.to_str().unwrap().ends_with(".rs") {
+	return;
+    }
+
+    let src = fs::read_to_string(&path).expect("unable to read file");
+    let syntax = syn::parse_file(&src).expect("unable to parse file");
+
+    for item in syntax.items {
+        match item {
+	    Impl(im) => print_pub_unsafe_and_unsafe_containing_fns(im),
+	    Trait(t) => print_trait_unsafe_containing_fns(t),
+	    _ => (),
+        }
+    }
+}
+
+fn handle_dir(path:&Path) -> io::Result<()> {
+    // https://users.rust-lang.org/t/testable-way-to-iterate-over-a-directory/81440
+    let mut dirs = Vec::new();
+    let mut dir_index = 0;
+    let mut dir_reader = fs::read_dir(path)?;
+    let mut had_files = false;
+    loop {
+	match dir_reader.next() {
+	    Some(entry) => {
+		let cur_path = entry?.path();
+		had_files = true;
+		if cur_path.is_dir() {
+		    dirs.push(cur_path);
+		    continue;
+		}
+
+		if cur_path.is_file() {
+		    handle_file(&cur_path);
+		    continue;
+		}
+	    }
+	    _ => {
+		if !had_files && !dirs.is_empty() {
+		    handle_file(&dirs[(dir_index - 1).max(0)].to_owned());
+		}
+		if dir_index == dirs.len() {
+		    break;
+		}
+		dir_reader = dirs[dir_index].read_dir()?;
+		had_files = false;
+		dir_index += 1;
+	    }
+	}
+    }
+
+    Ok(())
+}
+
 fn main() {
     let mut args = env::args();
     let _ = args.next(); // executable name
 
     if args.len() == 0 {
-        eprintln!("Usage: unsafe-finder [path/to/filename.rs]*");
+        eprintln!("Usage: unsafe-finder [directory | filename.rs]*");
         process::exit(1);
     }
 
-    for filename in args {
-	let src = fs::read_to_string(&filename).expect("unable to read file");
-	let syntax = syn::parse_file(&src).expect("unable to parse file");
-	println!("# Unsafe usages in file {}", filename);
-
-	for item in syntax.items {
-            match item {
-		Impl(im) => print_pub_unsafe_and_unsafe_containing_fns(im),
-		Trait(t) => print_trait_unsafe_containing_fns(t),
-		_ => (),
-            }
+    for arg in args {
+	let path = Path::new(&arg);
+	if path.is_file() {
+	    handle_file(&path);
+	} else if path.is_dir() {
+	    handle_dir(&path).unwrap();
 	}
     }
 }
